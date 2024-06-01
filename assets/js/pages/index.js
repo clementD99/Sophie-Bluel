7c455e539936680b7ce0d86caff7c7f38b44eb2b
Clément Desmur <clement.desmur@gmail.com>
2024-06-01 12:20:00 +0200

    Suppression de commentaires

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -3,12 +3,9 @@ import { getWorks, getCategories } from "../api.js";
 import { displayWorks } from "../works.js";
 import { addWorksModal1 } from "../modale.js";
 import { changeInterfaceIfAuthenticated } from "../auth.js";
-// import { suppressionProjetModale } from "../modale.js";
-// import { gestionSuppressionProjet } from "../modale.js";
 
 changeInterfaceIfAuthenticated();
 
-console.log("TEST");
 const works = await getWorks();
 const categories = await getCategories();
 console.log(categories);
@@ -16,6 +13,3 @@ console.log(works);
 displayWorks(works);
 initFilters(categories, works);
 addWorksModal1(works);
-// suppressionProjetModale();
-// gestionSuppressionProjet();
-// addEventsToFilters(works, parent);
